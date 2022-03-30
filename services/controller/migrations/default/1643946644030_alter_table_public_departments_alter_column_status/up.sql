5853eda850b3c48b9bb82674dd2ca6dca7feaed3
vodoanhoanglong <vodoanhoanglong10a9@gmail.com>
2022-03-30 14:29:53 +0700

    Update DD + permission

--- services/controller/migrations/default/1643946644030_alter_table_public_departments_alter_column_status/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."departments" alter column "status" set default '0';
--- services/controller/migrations/default/1644031757627_alter_table_public_files_alter_column_sharedDepartment/down.sql
+++
@@ -1,2 +0,0 @@
-alter table "public"."files" alter column "sharedDepartment" set default 'false';
-ALTER TABLE "public"."files" ALTER COLUMN "sharedDepartment" TYPE boolean;
